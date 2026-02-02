f275bed557a7ce530a4367efae84bf2e787dc120
YourSempay <BRAWLWIN123@mail.ru>
2026-02-02 15:27:40 +1000

    Лехенда

---
+++ SystemControlHuman/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SystemControlHuman.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ SystemControlHuman/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SystemControlHuman.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
